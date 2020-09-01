023054877e799a576b9141f795f47f68de45bc61
leo <leo09613257@gmail.com>
2020-09-02 00:48:50 +0800

    add new function

--- num_guess_game.py
+++ num_guess_game.py
@@ -1,17 +1,24 @@
 import random 
 
-random = random.randint(1,100)
+
+start = int(input("Start number : ")) #step3 : set guess range , and the range you can set between 1 to 1000
+end = int(input("End number : ")) 
+random = random.randint(start,end) 
+count = 0
 
 while True :
     num = int(input("Guess number : "))
+    count += 1
     if num == random :
         print("Congratuaion !")
+        print("you total guess" , count , "times" ) #step2 also
         break
-    elif random > num and not (num>100) :
+    elif random > num and not (num>1000) :
         print("you must guess upper !")
-    elif random < num and not (num>100) :
+    elif random < num and not (num>1000) :   
         print("you must guess lower !")
-    elif num > 100 or num < 0 :
+    elif num > 1000 or num < 0 :   
         print("not include the range !")
     else :
         print("error")
+    print("you guess" , count , "times")  #step2 : set how many time you have guessed
